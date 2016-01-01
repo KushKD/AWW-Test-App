476c372c691b49597e9fcf088620424522eaaa8e
kuush <kushkumardhawan@gmail.com>
2016-01-01 12:05:26 +0530

    Update

--- app/src/main/java/com/dithp/aadhaar/aemc_hp/Constants.java
+++ app/src/main/java/com/dithp/aadhaar/aemc_hp/Constants.java
@@ -7,6 +7,26 @@ package com.dithp.aadhaar.aemc_hp;
 public class Constants {
 
 
-
+    public static final String url_Generic = "";
+    public static final String url_Delemetre = "";
+    public static final String methord_Login = "";
+    public static final String methord_otp = "";
+    public static final String Login_Success = "";
+    public static final String OTP_Successfull = "";
+    public static final String DATASENT = "";
+    public static final String URL_BASE = "";
+    public static final String JSON_Save = "";
+    public static final String JSON_OTP = "";
+    public static final String JSON_LOGIN = "";
+    public static final String Error1 = "Something Went Wrong";
+    public static final String SERVER_MESSAGE = "Connecting to Server .. Please Wait";
+    public static final String AError1 ="Please Enter a valid Aadhaar No.";
+    public static final String AError2 = "Please enter your Aadhaar No." ;
+    public static final String OTPError1 = "Please Ensure that the OTP is Valid";
+    public static final String OTPError2 = "Please enter the OTP that has been sent to your Mobile number";
+    public static final String EXIT = "Press the back button once again to close the application";
+    public static final String Enroll = "Please Enter the total no of enrollments done today ";
+    public static final String PError1 = "Enter a Valid Phone Number";
+    public static final String PError2 = "Please Enter Your Phone Number";
 
 }
